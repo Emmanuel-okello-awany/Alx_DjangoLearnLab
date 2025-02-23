86b669b114399149a83db94bc42f9f419f36a321
Emmanuel-okello-awany <2300811768@std.kyu.ac.ug>
2025-02-23 23:33:12 +0300

    code corrected

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, home ,login_view, logout_view,register_view # Ensure `list_books` is imported
-
+from .views import list_books, LibraryDetailView, home ,login_view # Ensure `list_books` is imported
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
     path("", home, name="home"),  # Add this line for the default homepage
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
-    path("register/", register_view, name="register"),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+
 ]
+
+
+
+
