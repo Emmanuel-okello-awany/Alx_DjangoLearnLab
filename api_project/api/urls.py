9f0c534757201a8f661502a5cf8a84272a041d29
Emmanuel-okello-awany <2300811768@std.kyu.ac.ug>
2025-03-14 02:57:40 +0300

    Implemented Authentication and Permissions in the api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet, BookList
+from rest_framework.authtoken.views import obtain_auth_token
 
 # Create a router and register the viewset
 router = DefaultRouter()
@@ -12,5 +13,8 @@ urlpatterns = [
 
     # Register the ViewSet routes
     path('', include(router.urls)),  # This includes all routes for the BookViewSet
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
+
+
 
