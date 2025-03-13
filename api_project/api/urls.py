51f76878834448ae323e82efe586bd27616e7203
Emmanuel-okello-awany <2300811768@std.kyu.ac.ug>
2025-03-14 02:14:02 +0300

    Implemented CRUD operations with ViewSets and Routers in DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, BookList
+
+# Create a router and register the viewset
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Existing route for list-only view
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Register the ViewSet routes
+    path('', include(router.urls)),  # This includes all routes for the BookViewSet
 ]
+
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+    
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing book instances.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
